d6553308e36e8999df0de599d9ab270b5e0ddb04
Yasu <yasu@pixiv.co.jp>
2018-09-18 20:21:58 +0900

    remove whitespace

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ if has "git"; then
 # 使えない場合は curl か wget を使用する
 elif has "curl" || has "wget"; then
     tarball="https://github.com/$USER/dotfiles/archive/master.tar.gz"
-    
+
     # どっちかでダウンロードして，tar に流す
     if has "curl"; then
         curl -L "$tarball"
@@ -23,7 +23,7 @@ elif has "curl" || has "wget"; then
         wget -O - "$tarball"
 
     fi | tar xv -
-    
+
     # 解凍したら，DOTPATH に置く
     mv -f dotfiles-master "$DOTPATH"
 else
